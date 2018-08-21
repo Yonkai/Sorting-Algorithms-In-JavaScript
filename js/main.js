24636995d9b34172f77c44268ea08a2c08a44bc0
Frank <fgunsch.webdev@gmail.com>
2018-08-20 21:53:34 -0400

    bubblesort added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 (function(){
 
-//Bubble Sort
+function MasterSortingFunction(){
+    let array = [];
+
+    this.insert = function(item){
+        array.push(item);
+    };
+
+    this.toString = function(){
+        return array.join();
+    };
+
+    //Bubble Sort
+    this.bubbleSort = function(){
+        var length = array.length;
+        for(var i=0;i<length;i++){
+            for(var j=0;j<length-1-i;j++){
+                if(array[j]>array[j+1]){
+                    swap(array,j, j+1);
+                }
+            }
+        }
+    }
+
+    var swap = function(array, index1, index2){
+        var aux = array[index1];
+        array[index1]=array[index2];
+        array[index2]= aux;
+    }
+}
+
+//Set up in a prototype hierarchy hierarchy
 
 //Selection Sort
 
@@ -12,5 +42,5 @@
 
 //Quick Sort
 
-//Canvas Visualization 
+//Canvas Visualization, using sorting code to sort color circle
 })();
