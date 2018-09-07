4c3ad3fe054ee71a193a07765fe4d5563195a3e7
Frank <fgunsch.webdev@gmail.com>
2018-09-06 20:23:46 -0400

    added wheel code

--- js/main.js
+++ js/main.js
@@ -119,9 +119,23 @@
 
   }
 
+  //Color wheel creation (provided under MIT licence):
+  for (var i=0; i<360; i++) {
+    var color = document.createElement("span");
+    color.setAttribute("id", "d" + i);
+    color.style.backgroundColor = "hsl(" + i + ", 100%, 50%)"
+    color.style.msTransform = "rotate(" + i + "deg)"
+    color.style.webkitTransform = "rotate(" + i + "deg)"
+    color.style.MozTransform = "rotate(" + i + "deg)"
+    color.style.OTransform = "rotate(" + i + "deg)"
+    color.style.transform = "rotate(" + i + "deg)"
+    document.getElementById('colorwheel').appendChild(color);
+  };
+
   //Set up in a prototype hierarchy hierarchy
   //Canvas Visualization, using sorting code to sort color circle
   //Use the other color wheel instead it looks nicer.
   //Add options for controls for RESET,SPEED DELAY, NUMBER OF VALUES, and a DROP DOWN MENU FOR THE ALGORITHMS, and SOUND like in the youtube videos
   //Also display the big-o efficieny chart.
 })();
+
